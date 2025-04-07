4b2fc32a37f09f2d019087de838d27d63ce1010f
OpTick <pauliussuku@gmail.com>
2025-04-07 11:44:03 +0300

    docs + prepareCi

--- core/src/main/scala/yantl/NewTypeGivens.scala
+++ core/src/main/scala/yantl/NewTypeGivens.scala
@@ -1,17 +1,20 @@
 package yantl
 
-/** Provides standard Scala type class instances (`Numeric`, `Ordering`, etc.) for `NewType` wrappers. */
-trait NewTypeGivens extends scala.math.Integral.ExtraImplicits
-  with scala.math.Fractional.ExtraImplicits
+/** Provides standard Scala type class instances (`Numeric`, `Ordering`, etc.)
+  * for `NewType` wrappers.
+  */
+trait NewTypeGivens
+    extends scala.math.Integral.ExtraImplicits
+    with scala.math.Fractional.ExtraImplicits
 object NewTypeGivens {
   given newTypeOrdering[TUnderlying, TWrapper](using
-    yantl.Newtype.WithType[TUnderlying, TWrapper],
-    Ordering[TUnderlying]
+      yantl.Newtype.WithType[TUnderlying, TWrapper],
+      Ordering[TUnderlying]
   ): Ordering[TWrapper] = Ordering.by(_.unwrap)
 
   given newTypeNumeric[TUnderlying, TWrapper](using
-    newType: yantl.Newtype.WithUnvalidatedType[TUnderlying, TWrapper],
-    numeric: Numeric[TUnderlying]
+      newType: yantl.Newtype.WithUnvalidatedType[TUnderlying, TWrapper],
+      numeric: Numeric[TUnderlying]
   ): Numeric[TWrapper] = new Numeric[TWrapper] {
     override def plus(x: TWrapper, y: TWrapper): TWrapper =
       newType.make.unsafe(numeric.plus(x.unwrap, y.unwrap))
@@ -35,12 +38,13 @@ object NewTypeGivens {
     override def toLong(x: TWrapper): Long = numeric.toLong(x.unwrap)
     override def toFloat(x: TWrapper): Float = numeric.toFloat(x.unwrap)
     override def toDouble(x: TWrapper): Double = numeric.toDouble(x.unwrap)
-    override def compare(x: TWrapper, y: TWrapper): Int = numeric.compare(x.unwrap, y.unwrap)
+    override def compare(x: TWrapper, y: TWrapper): Int =
+      numeric.compare(x.unwrap, y.unwrap)
   }
 
   given newTypeIntegral[TUnderlying, TWrapper](using
-    newType: yantl.Newtype.WithUnvalidatedType[TUnderlying, TWrapper],
-    integral: Integral[TUnderlying]
+      newType: yantl.Newtype.WithUnvalidatedType[TUnderlying, TWrapper],
+      integral: Integral[TUnderlying]
   ): Integral[TWrapper] = new Integral[TWrapper] {
     override def quot(x: TWrapper, y: TWrapper): TWrapper =
       newType.make.unsafe(integral.quot(x.unwrap, y.unwrap))
@@ -51,22 +55,27 @@ object NewTypeGivens {
     private val numericInstance: Numeric[TWrapper] =
       newTypeNumeric(using newType, integral)
 
-    override def plus(x: TWrapper, y: TWrapper): TWrapper = numericInstance.plus(x, y)
-    override def minus(x: TWrapper, y: TWrapper): TWrapper = numericInstance.minus(x, y)
-    override def times(x: TWrapper, y: TWrapper): TWrapper = numericInstance.times(x, y)
+    override def plus(x: TWrapper, y: TWrapper): TWrapper =
+      numericInstance.plus(x, y)
+    override def minus(x: TWrapper, y: TWrapper): TWrapper =
+      numericInstance.minus(x, y)
+    override def times(x: TWrapper, y: TWrapper): TWrapper =
+      numericInstance.times(x, y)
     override def negate(x: TWrapper): TWrapper = numericInstance.negate(x)
     override def fromInt(x: Int): TWrapper = numericInstance.fromInt(x)
-    override def parseString(str: String): Option[TWrapper] = numericInstance.parseString(str)
+    override def parseString(str: String): Option[TWrapper] =
+      numericInstance.parseString(str)
     override def toInt(x: TWrapper): Int = numericInstance.toInt(x)
     override def toLong(x: TWrapper): Long = numericInstance.toLong(x)
     override def toFloat(x: TWrapper): Float = numericInstance.toFloat(x)
     override def toDouble(x: TWrapper): Double = numericInstance.toDouble(x)
-    override def compare(x: TWrapper, y: TWrapper): Int = numericInstance.compare(x, y)
+    override def compare(x: TWrapper, y: TWrapper): Int =
+      numericInstance.compare(x, y)
   }
 
   given newTypeFractional[TUnderlying, TWrapper](using
-    newType: yantl.Newtype.WithUnvalidatedType[TUnderlying, TWrapper],
-    fractional: Fractional[TUnderlying]
+      newType: yantl.Newtype.WithUnvalidatedType[TUnderlying, TWrapper],
+      fractional: Fractional[TUnderlying]
   ): Fractional[TWrapper] = new Fractional[TWrapper] {
     override def div(x: TWrapper, y: TWrapper): TWrapper =
       newType.make.unsafe(fractional.div(x.unwrap, y.unwrap))
@@ -74,16 +83,21 @@ object NewTypeGivens {
     private val numericInstance: Numeric[TWrapper] =
       newTypeNumeric(using newType, fractional)
 
-    override def plus(x: TWrapper, y: TWrapper): TWrapper = numericInstance.plus(x, y)
-    override def minus(x: TWrapper, y: TWrapper): TWrapper = numericInstance.minus(x, y)
-    override def times(x: TWrapper, y: TWrapper): TWrapper = numericInstance.times(x, y)
+    override def plus(x: TWrapper, y: TWrapper): TWrapper =
+      numericInstance.plus(x, y)
+    override def minus(x: TWrapper, y: TWrapper): TWrapper =
+      numericInstance.minus(x, y)
+    override def times(x: TWrapper, y: TWrapper): TWrapper =
+      numericInstance.times(x, y)
     override def negate(x: TWrapper): TWrapper = numericInstance.negate(x)
     override def fromInt(x: Int): TWrapper = numericInstance.fromInt(x)
-    override def parseString(str: String): Option[TWrapper] = numericInstance.parseString(str)
+    override def parseString(str: String): Option[TWrapper] =
+      numericInstance.parseString(str)
     override def toInt(x: TWrapper): Int = numericInstance.toInt(x)
     override def toLong(x: TWrapper): Long = numericInstance.toLong(x)
     override def toFloat(x: TWrapper): Float = numericInstance.toFloat(x)
     override def toDouble(x: TWrapper): Double = numericInstance.toDouble(x)
-    override def compare(x: TWrapper, y: TWrapper): Int = numericInstance.compare(x, y)
+    override def compare(x: TWrapper, y: TWrapper): Int =
+      numericInstance.compare(x, y)
   }
 }
